3eaf5f51994fdbd6aa1c4185c760a47fb8aa37ef
Talmagett <talmagett@gmail.com>
2024-05-01 16:56:19 +0600

    reverted player character controller, without jump, want to make it with event bus

--- Assets/Scripts/Map/PlayerCharacterController.cs
+++ Assets/Scripts/Map/PlayerCharacterController.cs
@@ -3,7 +3,6 @@ using Map.Interactions.Environment;
 using Sirenix.OdinInspector;
 using UnityEngine;
 using UnityEngine.InputSystem;
-using UnityEngine.Serialization;
 using Zenject;
 
 namespace Map
@@ -18,16 +17,7 @@ namespace Map
         [SerializeField] private float moveSpeed;
         [SerializeField] private float rotationSpeed;
 
-        [Range(0.0f, 0.3f)]
-        [SerializeField] private float RotationSmoothTime = 0.12f;
-        [SerializeField] private float speedChangeRate = 10.0f;
-        
-        [Space(10)]
-        [Tooltip("The height the player can jump")]
-        [SerializeField] float jumpHeight = 1.2f;
-        
         [SerializeField] private LayerMask groundLayers;
-        [SerializeField] private float jumpTimeout = 0.50f;
         [SerializeField] private float fallTimeout = 0.15f;
         [SerializeField] private float groundedOffset = -0.14f;
         [SerializeField] private float groundedRadius = 0.28f;
@@ -38,31 +28,13 @@ namespace Map
         private float interactRadius;
 
         private InputAction.CallbackContext _context;
-        
-        private float _jumpTimeoutDelta;
         private float _fallTimeoutDelta;
-        
         private bool _isMoving;
 
         private PlayerInputActions _playerInput;
         private Vector3 _targetDirection;
-        
-        private GameObject _mainCamera;
 
-        // player
-        private float _speed;
-        private float _animationBlend;
-        private float _targetRotation = 0.0f;
-        private float _rotationVelocity;
         private float _verticalVelocity;
-        private float _terminalVelocity = 53.0f;
-        
-        // animation IDs
-        private int _animIDSpeed = Animator.StringToHash("Speed");
-        private int  _animIDGrounded = Animator.StringToHash("Grounded");
-        private int _animIDJump = Animator.StringToHash("Jump");
-        private int _animIDFreeFall = Animator.StringToHash("FreeFall");
-        private int _animIDMotionSpeed = Animator.StringToHash("MotionSpeed");
 
         private void Start()
         {
@@ -136,7 +108,14 @@ namespace Map
             var targetRotation = Quaternion.LookRotation(_targetDirection, Vector3.up);
             transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, Time.deltaTime * rotationSpeed);
         }
-        
+
+        private void Move()
+        {
+            characterController.Move(_targetDirection.normalized * (moveSpeed * Time.deltaTime) +
+                                     new Vector3(0.0f, _verticalVelocity, 0.0f) * Time.deltaTime);
+            playerAnimator.SetBool(AnimationKey.GetAnimation(AnimationKey.Animation.IsMoving), _isMoving);
+        }
+
         private void Interact(InputAction.CallbackContext obj)
         {
             var hits = Physics.OverlapSphere(transform.position + Vector3.up, interactRadius);
@@ -151,132 +130,22 @@ namespace Map
                 transform.position.z);
             grounded = Physics.CheckSphere(spherePosition, groundedRadius, groundLayers,
                 QueryTriggerInteraction.Ignore);
-            playerAnimator.SetBool(_animIDGrounded, grounded);
         }
-        
-        private void Move()
-        {
-            // set target speed based on move speed, sprint speed and if sprint is pressed
-            float targetSpeed = moveSpeed;
-
-            // a simplistic acceleration and deceleration designed to be easy to remove, replace, or iterate upon
-
-            // note: Vector2's == operator uses approximation so is not floating point error prone, and is cheaper than magnitude
-            // if there is no input, set the target speed to 0
-            if (_targetDirection == Vector3.zero) targetSpeed = 0.0f;
-
-            // a reference to the players current horizontal velocity
-            float currentHorizontalSpeed = new Vector3(characterController.velocity.x, 0.0f, characterController.velocity.z).magnitude;
-
-            float speedOffset = 0.1f;
-            float inputMagnitude = _input.analogMovement ? _input.move.magnitude : 1f;
-
-            // accelerate or decelerate to target speed
-            if (currentHorizontalSpeed < targetSpeed - speedOffset ||
-                currentHorizontalSpeed > targetSpeed + speedOffset)
-            {
-                // creates curved result rather than a linear one giving a more organic speed change
-                // note T in Lerp is clamped, so we don't need to clamp our speed
-                _speed = Mathf.Lerp(currentHorizontalSpeed, targetSpeed * inputMagnitude,
-                    Time.deltaTime * speedChangeRate);
-
-                // round speed to 3 decimal places
-                _speed = Mathf.Round(_speed * 1000f) / 1000f;
-            }
-            else
-            {
-                _speed = targetSpeed;
-            }
-
-            _animationBlend = Mathf.Lerp(_animationBlend, targetSpeed, Time.deltaTime * speedChangeRate);
-            if (_animationBlend < 0.01f) _animationBlend = 0f;
-
-            // normalise input direction
-            Vector3 inputDirection = new Vector3(_targetDirection.x, 0.0f, _targetDirection.y).normalized;
-
-            // note: Vector2's != operator uses approximation so is not floating point error prone, and is cheaper than magnitude
-            // if there is a move input rotate player when the player is moving
-            if (_targetDirection != Vector3.zero)
-            {
-                _targetRotation = Mathf.Atan2(inputDirection.x, inputDirection.z) * Mathf.Rad2Deg +
-                                  _mainCamera.transform.eulerAngles.y;
-                float rotation = Mathf.SmoothDampAngle(transform.eulerAngles.y, _targetRotation, ref _rotationVelocity,
-                    RotationSmoothTime);
-
-                // rotate to face input direction relative to camera position
-                transform.rotation = Quaternion.Euler(0.0f, rotation, 0.0f);
-            }
 
 
-            Vector3 targetDirection = Quaternion.Euler(0.0f, _targetRotation, 0.0f) * Vector3.forward;
-
-            // move the player
-            characterController.Move(targetDirection.normalized * (_speed * Time.deltaTime) +
-                             new Vector3(0.0f, _verticalVelocity, 0.0f) * Time.deltaTime);
-
-            // update animator if using character
-            playerAnimator.SetFloat(_animIDSpeed, _animationBlend);
-            playerAnimator.SetFloat(_animIDMotionSpeed, inputMagnitude);
-        }
-        
         private void JumpAndGravity()
         {
             if (grounded)
             {
-                // reset the fall timeout timer
                 _fallTimeoutDelta = fallTimeout;
-
-                // update animator if using character
-                    playerAnimator.SetBool(_animIDJump, false);
-                    playerAnimator.SetBool(_animIDFreeFall, false);
-
-                // stop our velocity dropping infinitely when grounded
-                if (_verticalVelocity < 0.0f)
-                {
-                    _verticalVelocity = -2f;
-                }
-
-                // Jump
-                if (_input.jump && _jumpTimeoutDelta <= 0.0f)
-                {
-                    // the square root of H * -2 * G = how much velocity needed to reach desired height
-                    _verticalVelocity = Mathf.Sqrt(jumpHeight * -2f * gravity);
-
-                    // update animator if using character
-                        playerAnimator.SetBool(_animIDJump, true);
-                }
-
-                // jump timeout
-                if (_jumpTimeoutDelta >= 0.0f)
-                {
-                    _jumpTimeoutDelta -= Time.deltaTime;
-                }
+                if (_verticalVelocity < 0.0f) _verticalVelocity = -2f;
             }
             else
             {
-                // reset the jump timeout timer
-                _jumpTimeoutDelta = jumpTimeout;
-
-                // fall timeout
-                if (_fallTimeoutDelta >= 0.0f)
-                {
-                    _fallTimeoutDelta -= Time.deltaTime;
-                }
-                else
-                {
-                    // update animator if using character
-                    playerAnimator.SetBool(_animIDFreeFall, true);
-                }
-
-                // if we are not grounded, do not jump
-                _input.jump = false;
+                if (_fallTimeoutDelta >= 0.0f) _fallTimeoutDelta -= Time.deltaTime;
             }
 
-            // apply gravity over time if under terminal (multiply by delta time twice to linearly speed up over time)
-            if (_verticalVelocity < TerminalVelocity)
-            {
-                _verticalVelocity += gravity * Time.deltaTime;
-            }
+            if (_verticalVelocity < TerminalVelocity) _verticalVelocity += gravity * Time.deltaTime;
         }
     }
 }
